6dbcc3b1a69135167e59bd5c51fdf2abb6015aa6
Gabriel <g.ferrate.c@gmail.com>
2021-02-18 11:44:12 +0000

    -y

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update -y
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common \ 
-    -y
+    software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
